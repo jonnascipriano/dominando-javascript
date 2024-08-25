505b330f00a69aa844aa5cc290e27ac930bf1c99
Jonnas Cipriano <jonnascipriano@gmail.com>
2024-08-25 10:41:42 -0300

    ex001 concluido

---
+++ Fórmulas Matemáticas/ex001/script.js
@@ -0,0 +1,12 @@
+let num1 = parseFloat(prompt("Digite o primeiro número"))
+let num2 = parseFloat(prompt("Digite o segundo número"))
+
+let soma = num1 + num2
+let subtracao = num1 - num2
+let multiplicacao = num1 * num2
+let divisao = num1 / num2
+
+document.querySelector('p').innerHTML += `Soma: ${soma}<br>`
+document.querySelector('p').innerHTML += `Subtração: ${subtracao}<br>`
+document.querySelector('p').innerHTML += `Multiplicação: ${multiplicacao}<br>`
+document.querySelector('p').innerHTML += `Divisão: ${divisao}<br>`
