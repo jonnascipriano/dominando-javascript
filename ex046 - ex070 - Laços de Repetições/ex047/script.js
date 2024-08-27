5184de04cbf403a2b8734bf5bbfe7061dc94623d
Jonnas Cipriano <jonnascipriano@gmail.com>
2024-08-27 15:58:21 -0300

    concluído
    Escreva um programa que exiba os números de 1 a 10 utilizando um laço de repetição

---
+++ ex046 - ex070 - Laços de Repetições/ex047/script.js
@@ -0,0 +1,11 @@
+// Escreva um programa que exiba os números de 1 a 10 utilizando um laço de repetição
+
+let i = 1
+
+// Utilizando um laço de repetição while
+// enquanto i for menor ou igual a 10, exiba o valor de i e incremente o valor de i +1
+while (i <= 100){
+    document.querySelector('p').innerHTML += `${i}<br>`
+    // console.log(i)
+    i++
+}
