ce06d83e1c4736c8c8e0e75c0b35a89d35bdedea
Jonnas Cipriano <jonnascipriano@gmail.com>
2024-08-27 16:03:05 -0300

    concluido

--- ex046 - ex070 - Laços de Repetições/ex047/ex047-2/script.js
+++ ex046 - ex070 - Laços de Repetições/ex047/ex047-2/script.js
@@ -1,4 +1,4 @@
-// Escreva um programa que exiba os números de 1 a 10 utilizando um laço de repetição
+// Escreva um programa que exiba os números de 1 a 100 utilizando um laço de repetição
 
 for(let i = 1; i <= 100; i++){
     document.querySelector('p').innerHTML += `${i}<br>`
